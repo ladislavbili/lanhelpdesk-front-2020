739bb8888e3d9e33c11df2a03daa7be1e3c9b219
ladislavbili <l.bilisics@gmail.com>
2020-09-16 23:52:48 +0200

    LB - fix v2

--- src/configs/restAPI.js
+++ src/configs/restAPI.js
@@ -1,5 +1,5 @@
 const testingREST = false;
-const protectedREST = 'http://nodejs01.lanhelpdesk.com';
+const protectedREST = 'http://212.89.238.104:4000';
 const localREST = 'http://localhost:4000';
 
 export const REST_URL = testingREST ? localREST : protectedREST
