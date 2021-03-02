73b11d5c45c4a3fd40a8e7dfe6662c7e9675e8a3
Senkovičová Soňa <36665069+senkovicova1@users.noreply.github.com>
2021-03-02 18:22:51 +0100

    SS-02.03.2021-task10367-03

--- src/helpdesk/components/repeat/repeatsList.js
+++ src/helpdesk/components/repeat/repeatsList.js
@@ -50,81 +50,104 @@ export default function RepeatList( props ) {
     return ( <Loading /> )
   }
 
+  /*  */
   return (
-    <div className="content">
-      <div className="row m-0 p-0 taskList-container">
-        <div className="commandbar p-l-20">
-          <div className="search-row">
-            <div className="search">
-              <button className="search-btn" type="button">
-                <i className="fa fa-search" />
-              </button>
-              <input
-                type="text"
-                className="form-control search-text search"
-                value={repeatFilter}
-                onChange={(e)=>setRepeatFilter(e.target.value)}
-                placeholder="Search"
-                />
+    <div className="content-page">
+      <div className="content" style={{ paddingTop: 0 }}>
+        <div className="row m-0">
+          <div className="flex" >
+
+            <div className="task-list-commandbar p-l-30">
+              <div className="breadcrum-bar center-hor">
+                <div className="breadcrumbs">
+                  <h2>
+                    Repeats
+                  </h2>
+                </div>
+              </div>
             </div>
+
+            <div className="full-width scroll-visible fit-with-header-and-commandbar-4 task-container">
+
+              <div className={classnames("d-flex", "h-60px", "flex-row")}>
+                <div
+                  className={classnames(
+                    "m-l-30",
+                    "search-row",
+                  )}
+                  >
+                  <div className="search">
+                    <button className="search-btn" type="button">
+                      <i className="fa fa-search flip" />
+                    </button>
+                    <input
+                      type="text"
+                      className="form-control search-text"
+                      value={repeatFilter}
+                      onChange={(e)=>setRepeatFilter(e.target.value)}
+                      placeholder="Search"
+                      />
+                  </div>
+                </div>
+              </div>
+
+              <table className = "table" >
+                <thead>
+                  <tr>
+                    <th width="5%">Status</th>
+                    <th>Title</th>
+                    <th >Repeat timing</th>
+                    <th >Project</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    repeatsData.repeats.filter( ( repeat ) => repeat.repeatTemplate.title.toLowerCase()
+                    .includes( repeatFilter.toLowerCase() ) )
+                    .map( ( repeat ) =>
+                    <tr key={repeat.id}
+                      className="clickable"
+                      onClick={()=>{ setOpenRepeat(repeat) }}
+                      >
+                      <td>
+                        <span
+                          className="label label-info"
+                          style={{
+                            backgroundColor: repeat.repeatTemplate.status
+                            ? repeat.repeatTemplate.status.color
+                            : "white"
+                          }}
+                          >
+                          {
+                            repeat.repeatTemplate.status
+                            ? repeat.repeatTemplate.status.title
+                            : "No status"
+                          }
+                        </span>
+                      </td>
+                      <td>
+                        {repeat.repeatTemplate.title}
+                      </td>
+                      <td>
+                        {("Opakovať každý " + repeat.repeatEvery + ' ' + intervals.find((interval) => interval.value === repeat.repeatInterval ).title)}
+                      </td>
+                      <td>
+                        {repeat.repeatTemplate.project.title}
+                      </td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+            <Repeat
+              isOpen={openRepeat !== null}
+              repeat={openRepeat}
+              closeModal={ () => setOpenRepeat( null ) }
+              />
           </div>
         </div>
-        <div className="p-t-9 p-r-10 p-l-10 scroll-visible fit-with-header-and-commandbar full-width">
-          <h2 className=" p-l-10 p-b-10 ">
-            Repeats
-          </h2>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Repeat timing</th>
-                <th>Project</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {repeatsData.repeats.filter( (repeat) => repeat.repeatTemplate.title.toLowerCase().includes(repeatFilter.toLowerCase())).map((repeat) =>
-                <tr key={repeat.id}
-                  className={classnames (
-                    "clickable",
-                  )}
-                  onClick={()=>{ setOpenRepeat(repeat) }}>
-                  <td>
-                    {repeat.repeatTemplate.title}
-                  </td>
-                  <td width="10%">
-                    {("Opakovať každý " + repeat.repeatEvery + ' ' + intervals.find((interval) => interval.value === repeat.repeatInterval ).title)}
-                  </td>
-                  <td>
-                    {repeat.repeatTemplate.project.title}
-                  </td>
-                  <td>
-                    <span
-                      className="label label-info"
-                      style={{
-                        backgroundColor: repeat.repeatTemplate.status
-                        ? repeat.repeatTemplate.status.color
-                        : "white"
-                      }}
-                      >
-                      {
-                        repeat.repeatTemplate.status
-                        ? repeat.repeatTemplate.status.title
-                        : "No status"
-                      }
-                    </span>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
       </div>
-      <Repeat
-        isOpen={openRepeat !== null}
-        repeat={ openRepeat }
-        closeModal={() => setOpenRepeat(null)}
-        />
     </div>
+  </div>
   );
 }
